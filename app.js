a78b99d4de9e82112b44fdab8228af08e18e66ed
Emiliano <emiliano.serafin@gmail.com>
2021-07-22 14:25:34 -0400

    refactored code into separate functions

--- app.js
+++ app.js
@@ -2,71 +2,62 @@
  * A Javascript Rock Paper Scissors game played in the console.
  */
 
+const startBtn = document.querySelector('#startGame');
+const pScore = document.querySelector('#pScore');
+const cScore = document.querySelector('#cScore');
+const pChoice = document.querySelector('#pChoice');
+const cChoice = document.querySelector('#cChoice');
+const msg = document.querySelector('#message');
+
+// store users selection for round
+let userPlay = "";
+let compPlay = "";
+
+// wait for user to click/start game
+startBtn.addEventListener('click', () => {
+    // get users action (rock paper or scissors)
+    playGame();
+})
+
+const promptUser = function () {
+    // ask user for their choice
+    userPlay = prompt("Please choose Rock, Paper or Scissors").toLowerCase();
+    // check to see if choice was valid
+    if (userPlay === "rock" || userPlay === "paper" || userPlay === "scissors") {
+        //if valid, display player choice on page
+        pChoice.innerHTML = `<h4>${userPlay.toUpperCase()}</h4>`;
+    } else {
+        //if not valid, alert user and re-prompt for selection
+        alert('Invalid Selection, please try again')
+        promptUser();
+    }
+
+}
+
 // function that returns 1 of 3 options (rock, paper or scissors) 
-let computerPlay = function () {
-    let choice = Math.floor(Math.random() * 3) + 1;
-
-    switch (choice) {
-        case 1:
-            return "rock";
-            break;
-        case 2:
-            return "paper";
-            break;
-        case 3:
-            return "scissors";
-            break;
-        default:
-            return "uh oh, something went wrong";
+const computerPlay = function () {
+    let choice = function () {
+        switch (Math.floor(Math.random() * 3) + 1) {
+            case 1:
+                choice = "Rock";
+                break;
+            case 2:
+                choice = "Paper";
+                break;
+            case 3:
+                choice = "Scissors";
+                break;
+            default:
+        }
+        return choice;
     }
+    cChoice.innerHTML = `<h4>${choice().toUpperCase()}</h4>`;
+    compPlay = choice;
 }
 
-function game(num) {
-
-    for (let i = 0; i < num; i++) {
-
-        console.log(playRound())
-        if (playRound() === "It's a tie! Try Again") {
-            i -= 1;
-        }
-    }
-
-
+const playGame = function () {
+    promptUser();
+    computerPlay();
 }
-
-const playRound = function () {
-
-    let playerSelection = prompt("please choose Rock, Paper or Scissors:").toLowerCase();
-    let computerSelection = computerPlay();
-
-    // first check if both players picked same play (tie)
-    if (playerSelection === computerSelection) {
-
-        return "It's a tie! Try Again"
-    }
-    // check who wins if player plays rock
-    if (playerSelection === "rock") {
-        if (computerSelection === "scissors") {
-            return "You win! Rock beats Scissors!";
-        } else {
-            return `You lose! ${computerSelection} beats Rock!`
-        }
-    }
-    if (playerSelection === "paper") {
-        if (computerSelection === "rock") {
-            return "You win! Paper beats Rock!";
-        } else {
-            return `You lose! ${computerSelection} beats Paper!`
-        }
-    }
-    if (playerSelection === "scissors") {
-        if (computerSelection === "paper") {
-            return "You win! Scissors beats Paper!";
-        } else {
-            return `You lose! ${computerSelection} beats Scissors!`
-        }
-    }
-}
-
 
 
